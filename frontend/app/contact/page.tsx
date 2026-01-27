aa0e46ba3c9ed72273ecdca5dd302477b50e9f3b
Fonker <oganga1aus@gmail.com>
2026-01-28 00:35:05 +0300

    added header

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -5,8 +5,14 @@ import { Header } from '@/components/frontpage/Header/Header';
 import {Footer} from '@/components/frontpage/Footer/Footer';
 
 export default function Page() {
+  const handleCartClick = () => {
+    // Handle cart click logic here
+    console.log('Cart clicked');
+  };
+
   return (
     <>
+      <Header onCartClick={handleCartClick} />
       <Contact />
       <Footer />
     </>
